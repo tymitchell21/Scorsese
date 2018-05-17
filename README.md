905275ddf4052098c4d788dc1df1afd35af4fd78
Tyler Ward <tymitchell21@icloud.com>
2018-05-17 13:49:25 -0400

    Changes README

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# User Directory
+# Scorsese Films
 
--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const form = document.querySelector('#filmForm')
 const films = []
 
+function createDeleteButton(film) {
+    
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'Delete'
+    deleteButton.addEventListener('click',() => {
+        const index = [...film.parentNode.childNodes].indexOf(film)
+        film.parentNode.removeChild(film)
+        films.splice(index,1)
+
+        console.log(films)
+    })
+
+    film.appendChild(deleteButton)
+}
+
 form.addEventListener('submit', (ev)=>{
     ev.preventDefault()
     filmName = ev.target.title.value
 
     films.push(filmName)
 
-    const filmList = document.querySelector('#filmList')
-
     const film = document.createElement('li')
     film.textContent = filmName
 
+    createDeleteButton(film)
+
+    const filmList = document.querySelector('#filmList')
     filmList.appendChild(film)
-    createDeleteButton(filmList)
+
+    console.log(films)
 
     ev.target.reset()
 })
