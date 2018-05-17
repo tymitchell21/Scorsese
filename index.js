55eef57984ec5705b0ef9e11585d1c5d495639f8
Tyler Ward <tymitchell21@icloud.com>
2018-05-17 14:30:37 -0400

    Setup event handler for flickForm

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
-const form = document.querySelector('#filmForm')
-const films = []
+const app = {
+    init: function(formSelector) {
+        document
+         .querySelector(formSelector)
+         .addEventListener('submit', this.handleSubmit)
+    },
 
-function createDeleteButton(film) {
-    
-    const deleteButton = document.createElement('button')
-    deleteButton.textContent = 'Delete'
-    deleteButton.addEventListener('click',() => {
-        const index = [...film.parentNode.childNodes].indexOf(film)
-        film.parentNode.removeChild(film)
-        films.splice(index,1)
-
-        console.log(films)
-    })
-
-    film.appendChild(deleteButton)
-}
-
-form.addEventListener('submit', (ev)=>{
-    ev.preventDefault()
-    filmName = ev.target.title.value
-
-    films.push(filmName)
-
-    const film = document.createElement('li')
-    film.textContent = filmName
-
-    createDeleteButton(film)
-
-    const filmList = document.querySelector('#filmList')
-    filmList.appendChild(film)
-
-    console.log(films)
-
-    ev.target.reset()
-})
+    handleSubmit: function(ev) {
+        ev.preventDefault()
+        const f = ev.target
+        console.log(f.title.value)
+    },
+ }
+ 
+ app.init('#flickForm')
